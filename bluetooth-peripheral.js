6c8a59b2b025fc6718610a78aced0fb9a7901c5c
Bruno Fernandez-Ruiz <b@getnexar.com>
2015-12-28 11:25:11 +0000

    Make it easier to pair by sending zero data when idle

--- bluetooth-peripheral.js
+++ bluetooth-peripheral.js
@@ -5,6 +5,8 @@ var BluetoothPeripheral = function() {
   process.env['BLENO_DEVICE_NAME'] = 'WaterRower S4';
   this.primaryService = new CyclingPowerService();
   this.hrmService = new HrmService();
+  this.last_timestamp = 0;
+  this.stroke_count = 0;
   var self = this;
 
   bleno.on('stateChange', function(state) {
@@ -29,13 +31,30 @@ var BluetoothPeripheral = function() {
   });
 
   this.notify = function(event) {
-    //  console.log("[BLE] " + JSON.stringify(event));
+    // console.log("[BLE] " + JSON.stringify(event));
     self.primaryService.notify(event);
     self.hrmService.notify(event);
     if (!('watts' in event) && !('heart_rate' in event)) {
       console.log("unrecognized event: %j", event);
+    } else {
+      if ('stroke_count' in event) {
+        self.stroke_count = event.stroke_count;
+      }
+      self.last_timestamp = Date.now();
     }
   };
+
+  var ping = function() {
+    var TIMEOUT = 4000;
+    // send a zero event if we don't hear for 4 seconds (15 rpm)
+    if (Date.now() - self.last_timestamp > TIMEOUT) {
+      self.notify({'heart_rate': 0,
+                   'watts': 0,
+                   'stroke_count': self.stroke_count})
+    }
+    setTimeout(ping, TIMEOUT);
+  }
+  ping();
 };
 
 module.exports.BluetoothPeripheral = BluetoothPeripheral;
