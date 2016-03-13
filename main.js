50b0a012b4ba94be05ec57525866a05658ccdd02
Bruno Fernandez-Ruiz <b@getnexar.com>
2016-03-13 17:47:48 +0000

    Lazy load bluetooth

--- main.js
+++ main.js
@@ -1,74 +1,68 @@
-var main_ble = function(test_mode) {
-  var peripheral = require('ble-cycling-power');
+var S4 = require('./s4');
+var peripheral = require('ble-cycling-power');
+var network = require('./network');
+var usb = require('./usb-peripheral');
+
+var mainBle = function(testMode) {
   var ble = new peripheral.BluetoothPeripheral('WaterRower S4');
 
-  if (test_mode) {
-    var S4 = require('./s4');
+  if (testMode) {
     var rower = new S4();
     rower.fakeRower(ble.notify);
   } else {
-    var network = require('./network');
     var listener = new network.MessageListener(ble.notify);
     listener.start();
   }
 };
 
-var main_usb = function(test_mode) {
-  var network = require('./network');
-  var usb = require('./usb-peripheral');
-  var S4 = require('./s4');
+var mainUsb = function(callback, testMode) {
 
-  var broadcaster = new network.MessageBroadcaster();
-  broadcaster.start();
   var rower = new S4();
-
-  if (test_mode) {
-    rower.fakeRower(broadcaster.send);
+  if (testMode) {
+    rower.fakeRower(callback);
   } else {
     // monitor USB attach and detach
-    var peripheral = new usb.UsbPeripheral();
-    peripheral.monitor_wr(rower.startRower(broadcaster.send), rower.stopRower(rower));
+    var usbPeripheral = new usb.UsbPeripheral();
+    usbPeripheral.monitorWr(rower.startRower(callback), rower.stopRower(rower));
 
     rower.findPort().then(function() {
-      rower.startRower(broadcaster.send)();
+      rower.startRower(callback)();
     }, function() {
+      // wait till we get the right serial
       console.log('[Init] Awaiting WaterRower S4.2 to be connected to USB port');
     });
   }
 };
 
-var main_full = function(test_mode) {
-  var peripheral = require('ble-cycling-power');
-  var S4 = require('./s4');
-  var usb = require('./usb-peripheral');
+var main = function(args) {
+  var runMode = args[2];
+  var testMode = args[3] === '--test';
+  if (runMode === 'usb') {
+    var broadcaster;
+    var broadcasterNotify = function(event) {
+      if (!broadcaster) {
+        broadcaster = new network.MessageBroadcaster();
+        broadcaster.start();
+      }
 
-  var ble = new peripheral.BluetoothPeripheral('WaterRower S4');
-  var rower = new S4();
-  if (test_mode) {
-    rower.fakeRower(ble.notify);
+      broadcaster.send(event);
+    };
+
+    mainUsb(broadcasterNotify, testMode);
+  } else if (runMode === 'ble') {
+    mainBle(testMode);
   } else {
-    // monitor USB attach and detach
-    var peripheral = new usb.UsbPeripheral();
-    peripheral.monitor_wr(rower.startRower(ble.notify), rower.stopRower(rower));
+    var ble;
+    var bleNotify = function(event) {
+      if (!ble) {
+        ble = new peripheral.BluetoothPeripheral('WaterRower S4');
+      }
 
-    rower.findPort().then(function() {
-      rower.startRower(ble.notify)();
-    }, function() {
-      console.log('[Init] Awaiting WaterRower S4.2 to be connected to USB port');
-    });
+      ble.notify(event);
+    };
+
+    mainUsb(bleNotify, runMode === '--test');
   }
 };
 
-var main = function() {
-  var run_mode = process.argv[2];
-  var test_mode = process.argv[3] === '--test';
-  if (run_mode === 'usb') {
-    main_usb(test_mode);
-  } else if (run_mode === 'ble') {
-    main_ble(test_mode);
-  } else {
-    main_full(run_mode === '--test');
-  }
-};
-
-main();
+main(process.argv);
--- usb-peripheral.js
+++ usb-peripheral.js
@@ -1,32 +1,34 @@
 var usb = require('usb');
 
 var UsbPeripheral = function() {
-  this.monitor_wr = function(startRower, stopRower) {
-    var wr_usb_event = function(device) {
+  this.monitorWr = function(startRower, stopRower) {
+    var wrUsbEvent = function(device) {
       if (!device) {
         return false;
       }
-      console.log("[USB] Device, " +
-                  "Vendor id: " + device.deviceDescriptor.idVendor +
-                  ", Product id: ", device.deviceDescriptor.idProduct);
+
+      console.log('[USB] Device, ' +
+                  'Vendor id: ' + device.deviceDescriptor.idVendor +
+                  ', Product id: ', device.deviceDescriptor.idProduct);
       var idProduct = device.deviceDescriptor.idProduct;
       if (device.deviceDescriptor.idVendor  === 0x04d8 &&
           device.deviceDescriptor.idProduct === 0x000a) {
         return true;
       }
+
       return false;
     };
 
     usb.on('attach', function(device) {
-      if (wr_usb_event(device)) {
-        console.log("[Init] WaterRower-S4.2 Connected to USB hub controller");
+      if (wrUsbEvent(device)) {
+        console.log('[Init] WaterRower-S4.2 Connected to USB hub controller');
         startRower();
       }
     });
 
     usb.on('detach', function(device) {
-      if (wr_usb_event(device)) {
-        console.log("[End] WaterRower-S4.2 Disconnected from USB hub controller");
+      if (wrUsbEvent(device)) {
+        console.log('[End] WaterRower-S4.2 Disconnected from USB hub controller');
         stopRower();
       }
     });
